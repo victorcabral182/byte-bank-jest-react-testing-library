3a47eb77c0404469d5300e7e0389b9fea1bbed1f
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2024-04-16 21:17:56 -0300

    refactor: update to v2

--- src/componentes/Extrato/Transacoes/Transacoes.test.js
+++ src/componentes/Extrato/Transacoes/Transacoes.test.js
@@ -3,20 +3,28 @@ import Transacoes from './index'
 import estilos from '../Extrato.module.css'
 
 test('Deve renderizar o mesmo componente com props atualizadas', () => {
-  const transacao = { transacao: 'Depósito', valor: 100 }
+  const transacao = {
+    transacao: 'Depósito',
+    valor: 100,
+  }
   const { rerender } = render(
-    <Transacoes estilos={estilos} transacao={transacao} />,
+    <Transacoes estilos={estilos} transacao={transacao} />
   )
   const tipoTransacao = screen.getByTestId('tipoTransacao')
   const valorTransacao = screen.getByTestId('valorTransacao')
+
   expect(tipoTransacao).toHaveTextContent('Depósito')
   expect(valorTransacao).toHaveTextContent('R$ 100')
 
-  const novaTransacao = { transacao: 'Transferência', valor: 50 }
+  const novaTransacao = {
+    transacao: 'Transferência',
+    valor: 50,
+  }
 
   rerender(<Transacoes estilos={estilos} transacao={novaTransacao} />)
   const novoTipoTransacao = screen.getByTestId('tipoTransacao')
   const novoValorTransacao = screen.getByTestId('valorTransacao')
+
   expect(novoTipoTransacao).toHaveTextContent('Transferência')
-  expect(novoValorTransacao).toHaveTextContent('R$ 50')
+  expect(novoValorTransacao).toHaveTextContent('- R$ 50')
 })
