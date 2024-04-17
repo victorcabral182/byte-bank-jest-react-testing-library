2cd03b7a6438b4a285b0191f3e19de09da96c367
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2024-04-17 19:35:41 -0300

    feat: creating actions

---
+++ src/hooks/hooks.test.js
@@ -0,0 +1,13 @@
+import { render, renderHook, screen } from '@testing-library/react'
+import { useEffect, useState } from 'react'
+
+test('Hooks', () => {
+  const { result } = renderHook(() => {
+    const [nome, setNome] = useState()
+    useEffect(() => {
+      setNome('Alice')
+    }, [])
+    return nome
+  })
+  expect(result.current).toBe('Alice')
+})
